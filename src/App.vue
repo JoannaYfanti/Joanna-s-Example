a815741eddcd1155a546738dd2d5e8fa8bd47217
Ioanna Yfanti <Ioanna.Yfanti@awacademy.traning>
2022-12-08 16:04:37 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+import ExampleComponent from "@/components/ExampleComponent/ExampleComponent.vue";
+
+export default {
+  components: {ExampleComponent}
+}
+</script>
+
+<template>
+  <ExampleComponent></ExampleComponent>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ExampleComponent/ExampleComponent.vue
@@ -0,0 +1,27 @@
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "ExampleComponent",
+  data(){
+    return{
+      songs:["Toxicity", "Forest", "Colors"]
+    }
+  }
+
+})
+
+</script>
+
+<template>
+  <h1>My favourite songs</h1>
+<ul>
+  <li v-for="song in songs">{{song}}</li>
+</ul>
+</template>
+
+
+
+<style scoped>
+
+</style>
